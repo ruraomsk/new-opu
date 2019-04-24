2332a264210bae8b8d98e367109a4abb90c6a03d
unknown <D:\home\The Bat!>
2019-04-24 08:55:09 +0600

    Changed: измен способ преобразования в Bool

--- Function/NOTAND.cs
+++ Function/NOTAND.cs
@@ -13,11 +13,8 @@ namespace Function
 
         public override string calculate(List<string> values)
         {
-            //          bool result = bool.Parse(values[0]);
             bool result = Helper.ReadAsBool(values[0]);
             foreach (string val in values.Skip(1)) {
-
-//                bool b = bool.Parse(val);
                 bool b = Helper.ReadAsBool( val );
                 result &= b;
             }
